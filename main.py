5f5f53ff6f210f9cfa088573b0d72be6127ba4c6
Mark <143651678+markm101@users.noreply.github.com>
2024-07-18 13:18:42 -0400

    fixed commas being required at the end of inputs

--- main.py
+++ main.py
@@ -4,15 +4,11 @@ import operators
 
 print('1. Addition, 2. Subtraction, 3. Multiplication, 4. Division, 5. Exponents, 6. Roots ')
 choice = int(input("Enter a decided on option: "))
-
 if choice == 5:
-    list_nums = operators.string_to_list(input("Enter your base followed by your exponent (base, exponent,)  "))
+    list_nums = operators.string_to_list(input("Enter your base followed by your exponent (base, exponent)  ") + ',')
     operators.exp(list_nums)
 else:
-
-    list_nums = operators.string_to_list(input("Enter your chosen numbers, each followed by a comma (example: 12,3,45,1,)  "))
-
-
+    list_nums = operators.string_to_list((input("Enter your chosen numbers, each followed by a comma (example: 12,3,45,1)  ")) + ',')
     if choice == 1:  #user selected 1, add
         operators.add(list_nums)
 
